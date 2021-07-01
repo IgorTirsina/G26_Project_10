6feb08193b97e9c984e240da7168465e88b5b031
Ali Ak <aliakbartin@gmail.com>
2021-06-30 21:43:23 -0400

    initial commit

---
+++ src/test/java/Ali.java
@@ -0,0 +1,6 @@
+public class Ali {
+    public static void main(String[] args) {
+        System.out.println("Hi guys!");
+        System.out.println("Can i assign any of you to review? ");
+    }
+}
